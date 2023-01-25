76b07b017377a51f4bae4d8b33a953f39fa09397
Adam Tworkowski <adam.tworkowski2012@gmail.com>
2023-01-25 23:00:05 +0100

    Layout for roles

---
+++ MedicaERP.Web/Constans.cs
@@ -0,0 +1,9 @@
+﻿namespace MedicaERP.Web
+{
+    public static class Constans
+    {
+        public const string AdministratorRole = "Admin";
+        public const string DoctorRole = "Doctor";
+        public const string PatientRole = "Patient";
+    }
+}
